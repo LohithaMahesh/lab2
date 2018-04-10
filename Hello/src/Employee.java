e38146f82a0a7c31676fa0af3e7551e4e1c570b5
lsannasa <lsannasa@DIN51003267.corp.capgemini.com>
2018-04-10 15:46:56 +0530

    adding employee

---
+++ Hello/src/Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee
+{
+
+	public Employee(int id, String fName, String sName, double Salary, char Grade, String jDate)
+	{
+		System.out.println("The id of employee is       :"+ id);
+		System.out.println("The fName of employee is    :"+ fName);
+		System.out.println("The sName of employee is    :"+ sName);
+		System.out.println("The Salary of employee is   :"+ Salary);
+		System.out.println("The grade of employee is    :"+ Grade);
+		System.out.println("The jDate of employee is    :"+ jDate);
+		System.out.println("----------------------------------------------------------------");
+	}
+}
